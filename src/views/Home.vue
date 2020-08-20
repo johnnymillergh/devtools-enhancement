b1668e065a00a15d59c6d3ca88a704e92e590963
钟俊 (zhongjun02) <zhongjun02@zuche.com>
2020-08-20 16:59:14 +0800

    build(Project): initial project

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import * as DevtoolsDetector from 'devtools-detector'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    isOpen (isOpen) {
+      console.warn('isOpen', isOpen)
+      if (isOpen) {
+        alert(`isOpen: ${isOpen}`)
+        window.location.reload()
+      }
+    }
+  },
+  created () {
+    const view = document.createElement('div')
+    document.body.appendChild(view)
+    DevtoolsDetector.addListener(this.isOpen)
+    DevtoolsDetector.setDetectDelay(1000)
+    DevtoolsDetector.launch()
+  },
+  destroyed () {
+    DevtoolsDetector.removeListener(this.isOpen)
+    DevtoolsDetector.stop()
+  }
+}
+</script>
